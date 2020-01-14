be855c2502800694e8974d1f2089887af8aeb4b2
mavrikicecool <mavrikicecool@gmail.com>
2020-01-14 00:35:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-gpzmhy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gpzmhy)
---
+++ abc.js
@@ -0,0 +1,16 @@
+import React,  { Component } from 'react';
+
+//class bbc extends Component{
+  //render(){
+  
+  //  return  <h1>shashi</h1>
+  const abc = () =>{
+return React.createElement('div',{id:'abc',className:'bbx'},
+React.createElement('h1','null','shashi'))
+  }
+  
+    //}
+  
+//}
+
+export default abc;
